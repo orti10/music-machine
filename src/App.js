555f714ce9e319c695550804cc582751f3190351
orti10 <ortalh10@gmail.com>
2021-12-07 13:18:25 +0200

    Update html & App :receipt:

--- src/App.js
+++ src/App.js
@@ -44,73 +44,79 @@ const App = () => {
       audioTag.volume = volume;
       audioTag.pause();
       index++;
-    }, 100);
-    setTimeout(() => clearInterval(interval), 100 * audioClips.length - 1);
+    }, 50);
+    setTimeout(() => clearInterval(interval), 50 * audioClips.length - 1);
   };
 
   return (
-    <div className="text-center">
-      {/* map each clip, clip use for all the active keys */}
-      {audioClips.map((clip) => (
-        <Pad
-          key={clip.id}
-          clip={clip}
-          volume={volume}
-          setRecording={setRecording}
-        />
-      ))}
-      <br />
-      <div className="volume">
-        <h4>Volume</h4>
-        <input
-          type="range"
-          step="0.01"
-          //slide can be change (moving slide)
-          onChange={(e) => setVolume(e.target.value)}
-          value={volume}
-          max="1"
-          min="0"
-          className="w-45"
-        />
+  
+    <div className="page">
+      <input type="checkbox" id="themeSwitch" name="theme-switch" className="theme-switch__input" />
+      <label for="themeSwitch" className="theme-switch__label">
+        <span>Switch</span>
+      </label>
+      <main>
+        <div className="wrapper">
+          <h2>Welcome to the Loop Machine</h2> 
+          <p>© Written By Ortal</p>
+          <h3>Tap a key to start record</h3> 
+        </div>
+        <div className="text-center">
+          {/* map each clip, clip use for all the active keys */}
+          {audioClips.map((clip) => (
+            <Pad
+              key={clip.id}
+              clip={clip}
+              volume={volume}
+              setRecording={setRecording}
+            />
+          ))}
+          <br />  
+          <div className="volume">
+              <h4>Volume</h4>
+              <input
+                type="range"
+                step="0.01"
+                //slide can be change (moving slide)
+                onChange={(e) => setVolume(e.target.value)}
+                value={volume}
+                max="1"
+                min="0"
+                className="w-45"
+              />
+          </div>
+          <div className="bpm">
+              <h4>BPM</h4>
+              <input
+                type="range"
+                step="5"
+                onChange={(e) => setBpm(e.target.value)}
+                value={bpm}
+                max="120"
+                min="30"
+                className="w-45"
+              />
+          </div>
+          {/* write the keys that we used */}
+          <h3>{recording}</h3>
+            {
+              <>
+              {/* 'Play' the last session -success=green */}
+              <button onClick={playRecording} className="btn btn-success">Play-session</button>
+              &nbsp;&nbsp;
+              {/* 'Clear' the record and starts over -primary=blue */}
+              <button onClick={() => setRecording("")} className="btn btn-primary">Clear</button>
+              &nbsp;&nbsp;
+              {/* 'Play All' button should start all the music immediately -warning=yellow*/}
+              <button onClick={playAll} className="btn btn-warning">Play All</button>
+              &nbsp;&nbsp;
+              {/* 'Stop' button should stop all the music immediately -danger=red*/}
+              <button onClick={stopAll} className="btn btn-danger">Stop</button>
+              </>
+            }
       </div>
-      <div className="bpm">
-        <h4>BPM</h4>
-        <input
-          type="range"
-          step="5"
-          onChange={(e) => setBpm(e.target.value)}
-          value={bpm}
-          max="120"
-          min="30"
-          className="w-45"
-        />
-      </div>
-      {/* write the keys that we used */}
-      <h3>{recording}</h3>
-      {
-        <>
-          {/* 'Play' the last session -success=green */}
-          <button onClick={playRecording} className="btn btn-success">
-            Play-session
-          </button>
-          &nbsp;&nbsp;
-          {/* 'Clear' the record and starts over -primary=blue */}
-          <button onClick={() => setRecording("")} className="btn btn-primary">
-            Clear
-          </button>
-          &nbsp;&nbsp;
-          {/* 'Play All' button should start all the music immediately -warning=yellow*/}
-          <button onClick={playAll} className="btn btn-warning">
-            Play All
-          </button>
-          &nbsp;&nbsp;
-          {/* 'Stop' button should stop all the music immediately -danger=red*/}
-          <button onClick={stopAll} className="btn btn-danger">
-            Stop
-          </button>
-        </>
-      }
-    </div>
+    </main>
+  </div>    
   );
 };
 export default App;
